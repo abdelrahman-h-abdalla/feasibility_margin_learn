5dd08a1399d176e91ded9d03ec0425fb0cd05d52
Abdelrahman Abdalla <abdelrahman.abdalla@iit.it>
2023-06-08 14:42:31 +0100

    Add more information in robot model in Jetleg and add Anymal C URDF

--- jet_leg_common/jet_leg/robots/anymal/anymal_model.py
+++ jet_leg_common/jet_leg/robots/anymal/anymal_model.py
@@ -21,6 +21,15 @@ class AnymalModel:
             self.joint_torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
             self.contact_torque_limits = np.array([-1.5, 1.5])
 
+            x_nominal_b = 0.36
+            y_nominal_b = 0.2
+            z_nominal_b = -0.4
+            self.nominal_stance_LF = [x_nominal_b, y_nominal_b, z_nominal_b]
+            self.nominal_stance_RF = [x_nominal_b, -y_nominal_b, z_nominal_b]
+            self.nominal_stance_LH = [-x_nominal_b, y_nominal_b, z_nominal_b]
+            self.nominal_stance_RH = [-x_nominal_b, -y_nominal_b, z_nominal_b]
+            self.max_dev_from_nominal = [0.1, 0.1, 0.1]
+
         elif self.anymal_type == 'anymal_coyote':
             self.trunkMass = 52
             self.trunkInertia = np.array([[0.946438, -0.000938112, 0.00595386],
@@ -38,3 +47,12 @@ class AnymalModel:
             RH_tau_lim = [80.0, 80.0, 80.0]  # HAA, HFE, KFE
             self.joint_torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
             self.contact_torque_limits = np.array([-1.5, 1.5])
+
+            x_nominal_b = 0.29
+            y_nominal_b = 0.19
+            z_nominal_b = -0.46
+            self.nominal_stance_LF = [x_nominal_b, y_nominal_b, z_nominal_b]
+            self.nominal_stance_RF = [x_nominal_b, -y_nominal_b, z_nominal_b]
+            self.nominal_stance_LH = [-x_nominal_b, y_nominal_b, z_nominal_b]
+            self.nominal_stance_RH = [-x_nominal_b, -y_nominal_b, z_nominal_b]
+            self.max_dev_from_nominal = [0.2, 0.1, 0.1]
