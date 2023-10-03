9cebe235183fe13bdb3717d712663f33324da8e5
aabdalla <abdelrahman.abdalla@iit.it>
2023-10-03 15:21:14 +0200

    [fix] Change hyqreal torque limits to higher correct ones

--- jet_leg_common/jet_leg/robots/hyqreal/hyqreal_model.py
+++ jet_leg_common/jet_leg/robots/hyqreal/hyqreal_model.py
@@ -14,10 +14,10 @@ class HyqrealModel:
         HFE = Hip Flextion Extension
         KFE = Knee Flextion Extension
         '''
-        LF_tau_lim = [50.0, 100.0, 100.0]  # HAA, HFE, KFE
-        RF_tau_lim = [50.0, 100.0, 100.0]  # HAA, HFE, KFE
-        LH_tau_lim = [50.0, 100.0, 100.0]  # HAA, HFE, KFE
-        RH_tau_lim = [50.0, 100.0, 100.0]  # HAA, HFE, KFE
+        LF_tau_lim = [173.0, 208.0, 249.0]  # HAA, HFE, KFE
+        RF_tau_lim = [173.0, 208.0, 249.0]  # HAA, HFE, KFE
+        LH_tau_lim = [173.0, 208.0, 249.0]  # HAA, HFE, KFE
+        RH_tau_lim = [173.0, 208.0, 249.0]  # HAA, HFE, KFE
 
         self.joint_torque_limits = np.array([LF_tau_lim, RF_tau_lim, LH_tau_lim, RH_tau_lim])
         self.contact_torque_limits = np.array([-1, 1])
