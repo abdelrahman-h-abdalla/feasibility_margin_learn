Fix some examples and how numerical margin is computed and add new examples

jet_leg_common/examples/margin_jacobian/kinematic_margin_wrt_com_pos.py
@@ -24,7 +24,7 @@ plotter = Plotter()
 math = Math()
 
 ''' Set the robot's name (either 'hyq', 'hyqreal', 'anymal_boxy' or 'anymal_coyote')'''
-robot_name = 'hyq'
+robot_name = 'hyqreal'
 
 '''
 possible constraints for each foot:
@@ -55,10 +55,10 @@ projection = nonlinear_projection.NonlinearProjectionBretl(robot_name)
     informations needed for the computation of the IP'''
 params = IterativeProjectionParameters(robot_name)
 """ contact points in the World Frame"""
-LF_foot = np.array([0.34, 0.34, -0.52])
-RF_foot = np.array([0.36, -0.30, -0.52])
-LH_foot = np.array([-0.36, 0.32, -0.52])
-RH_foot = np.array([-0.36, -0.32, -0.52])
+LF_foot = np.array([0.44, 0.34, -0.55])  # Starting configuration w.o. trunk controller
+RF_foot = np.array([0.44, -0.34, -0.55])
+LH_foot = np.array([-0.44, 0.34, -0.55])
+RH_foot = np.array([-0.44, -0.34, -0.55])
 
 contactsWF = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
 
@@ -67,14 +67,22 @@ HAA = Hip Abduction Adduction
 HFE = Hip Flextion Extension
 KFE = Knee Flextion Extension
 '''
-LF_q_lim_max = [0.44, 1.2217, -0.3491]  # HAA, HFE, KFE
-LF_q_lim_min = [-1.22, -0.8727, -2.4435]  # HAA, HFE, KFE
-RF_q_lim_max = [0.44, 1.2217, -0.3491]  # HAA, HFE, KFE
-RF_q_lim_min = [-1.22, -0.8727, -2.4435]  # HAA, HFE, KFE
-LH_q_lim_max = [0.44, 0.8727, 2.4435]  # HAA, HFE, KFE
-LH_q_lim_min = [-1.22, -1.2217, 0.3491]  # HAA, HFE, KFE
-RH_q_lim_max = [0.44, 0.8727, 2.4435]  # HAA, HFE, KFE
-RH_q_lim_min = [-1.22, -1.2217, 0.3491]  # HAA, HFE, KFE
+# LF_q_lim_max = [0.44, 1.2217, -0.3491]  # HAA, HFE, KFE
+# LF_q_lim_min = [-1.22, -0.8727, -2.4435]  # HAA, HFE, KFE
+# RF_q_lim_max = [0.44, 1.2217, -0.3491]  # HAA, HFE, KFE
+# RF_q_lim_min = [-1.22, -0.8727, -2.4435]  # HAA, HFE, KFE
+# LH_q_lim_max = [0.44, 0.8727, 2.4435]  # HAA, HFE, KFE
+# LH_q_lim_min = [-1.22, -1.2217, 0.3491]  # HAA, HFE, KFE
+# RH_q_lim_max = [0.44, 0.8727, 2.4435]  # HAA, HFE, KFE
+# RH_q_lim_min = [-1.22, -1.2217, 0.3491]  # HAA, HFE, KFE
+LF_q_lim_max = [0.401, 2.181, -0.770]  # HAA, HFE, KFE
+LF_q_lim_min = [-0.733, 0.262, -2.770]  # HAA, HFE, KFE
+RF_q_lim_max = [0.401, 2.181, -0.770]  # HAA, HFE, KFE
+RF_q_lim_min = [-0.733, 0.262, -2.770]  # HAA, HFE, KFE
+LH_q_lim_max = [0.401, 2.181, -0.770]  # HAA, HFE, KFE
+LH_q_lim_min = [-0.733, 0.262, -2.770]  # HAA, HFE, KFE
+RH_q_lim_max = [0.401, 2.181, -0.770]  # HAA, HFE, KFE
+RH_q_lim_min = [-0.733, 0.262, -2.770]  # HAA, HFE, KFE
 joint_limits_max = np.array([LF_q_lim_max, RF_q_lim_max, LH_q_lim_max, RH_q_lim_max])
 joint_limits_min = np.array([LF_q_lim_min, RF_q_lim_min, LH_q_lim_min, RH_q_lim_min])
 
@@ -101,7 +109,7 @@ jac = KinematicJacobians(robot_name)
 
 
 '''Margin'''
-delta_pos_range = 0.4
+delta_pos_range = 0.3
 num_of_tests = 50
 delta_pos_range_vec = np.linspace(-delta_pos_range/2.0, delta_pos_range/2.0, num_of_tests)
 
@@ -118,7 +126,7 @@ print("DELTA Y")
 pos_margin_y, jac_com_pos_y = jac.plotMarginAndJacobianWrtComPosition(params_com_y, delta_pos_range_vec, 1) # dm / dy
 print("Jac_y:", jac_com_pos_y)
 print("DELTA Z")
-pos_margin_z, jac_com_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_com_z, delta_pos_range_vec, 2) # dm / dz
+# pos_margin_z, jac_com_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_com_z, delta_pos_range_vec, 2) # dm / dz
 
 # print("pos_margin_x:", pos_margin_x)
 # print("pos_margin_y:", pos_margin_y)
@@ -136,7 +144,7 @@ params_com_y_LH = deepcopy(params)
 params_com_z_LH = deepcopy(params)
 pos_margin_x_LH, jac_com_pos_x_LH = jac.plotMarginAndJacobianWrtComPosition(params_com_x_LH, delta_pos_range_vec, 0) # dm / dx
 pos_margin_y_LH, jac_com_pos_y_LH = jac.plotMarginAndJacobianWrtComPosition(params_com_y_LH, delta_pos_range_vec, 1) # dm / dy
-pos_margin_z_LH, jac_com_pos_z_LH = jac.plotMarginAndJacobianWrtComPosition(params_com_z_LH, delta_pos_range_vec, 2) # dm / dz
+# pos_margin_z_LH, jac_com_pos_z_LH = jac.plotMarginAndJacobianWrtComPosition(params_com_z_LH, delta_pos_range_vec, 2) # dm / dz
 
 ### Plotting
 

jet_leg_common/examples/margin_jacobian/plot_learned_com_kinematic_margin_jac_hyqreal_1111.py
@@ -21,36 +21,36 @@ from jet_leg_common.jet_leg.optimization.jacobians_kinematics import KinematicJa
 robot_name = 'hyqreal'
 
 num_of_tests = 25
-delta_pos_range_x = 0.6
-delta_pos_range_y = 0.4
+delta_pos_range_x = 1.
+delta_pos_range_y = 1.
 delta_pos_range_vec_x = np.linspace(-delta_pos_range_x/2.0, delta_pos_range_x/2.0, num_of_tests)
 delta_pos_range_vec_y = np.linspace(-delta_pos_range_y/2.0, delta_pos_range_y/2.0, num_of_tests)
 
 def plot_learnt_margin():
 
     paths = ProjectPaths()
-    # dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/1111', robot_name=robot_name)
-    # model_directory = '/hyqreal/seperate(kinematic)/1111/'
-    dataset_handler = TrainingDataset('../trained_models/final/stability_margin', robot_name=robot_name)
-    model_directory = '/final/stability_margin/'
+    dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/1111', robot_name=robot_name, in_dim=40)
+    model_directory = '/hyqreal/seperate(kinematic)/1111/'
     model_directory = paths.TRAINED_MODELS_PATH + model_directory
     print("model directory: ", model_directory)
-    network = MultiLayerPerceptron(in_dim=40, out_dim=1, hidden_layers=[512,256,128], activation='softsign', dropout=0.0)
+    network = MultiLayerPerceptron(in_dim=40, out_dim=1, hidden_layers=[256,256,128], activation='relu', dropout=0.0)
     # network.load_state_dict(torch.load(model_directory + 'network_state_dict.pt'))
     network.load_params_from_txt(model_directory + 'network_parameters_hyqreal.txt')
     network.eval()
 
-    ## Margin LF X
+    ## Margin X
     out_array_x = []
     jac_array_x = []
+    euler_angles = np.array([0., 0.0, 0.0])
+
     for delta in delta_pos_range_vec_x:
         network_input = np.concatenate([
             np.array([0.0, 0.0, 1.0]),
             np.zeros(12),
-            np.array([0.44 - delta, 0.34, -0.55]),
-            np.array([0.44 - delta, -0.34, -0.55]),
-            np.array([-0.44 - delta, 0.34, -0.55]),
-            np.array([-0.44 - delta, -0.34, -0.55]),
+            np.array([0.44 - delta, 0.34, -0.5]),
+            np.array([0.44 - delta, -0.34, -0.5]),
+            np.array([-0.44 - delta, 0.34, -0.5]),
+            np.array([-0.44 - delta, -0.34, -0.5]),
             np.array([0.6]),
             np.array([0.0, 0.0, 1.0] * 4)
         ])
@@ -65,18 +65,19 @@ def plot_learnt_margin():
         unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
         # jac_array_x.append(unnormalized_gradient[15])
         jac_array_x.append(-1 * (unnormalized_gradient[15] + unnormalized_gradient[18] + unnormalized_gradient[21] + unnormalized_gradient[24]))
+        # jac_array_x.append(-1 * (unnormalized_gradient[3] + unnormalized_gradient[6] + unnormalized_gradient[9] + unnormalized_gradient[12]))
 
-    ## Margin LF Y
+    ## Margin Y
     out_array_y = []
     jac_array_y = []
     for delta in delta_pos_range_vec_y:
         network_input = np.concatenate([
             np.array([0.0, 0.0, 1.0]),
             np.zeros(12),
-            np.array([0.44 , 0.34 - delta, -0.55]),
-            np.array([0.44 , -0.34 - delta, -0.55]),
-            np.array([-0.44, 0.34 - delta, -0.55]),
-            np.array([-0.44, -0.34 - delta, -0.55]),
+            np.array([0.44 , 0.34 - delta, -0.5]),
+            np.array([0.44 , -0.34 - delta, -0.5]),
+            np.array([-0.44, 0.34 - delta, -0.5]),
+            np.array([-0.44, -0.34 - delta, -0.5]),
             np.array([0.6]),
             np.array([0.0, 0.0, 1.0] * 4)
         ])
@@ -91,6 +92,35 @@ def plot_learnt_margin():
         unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
         # jac_array_y.append(unnormalized_gradient[16])
         jac_array_y.append(-1 * (unnormalized_gradient[16] + unnormalized_gradient[19] + unnormalized_gradient[22] + unnormalized_gradient[25]))
+        # jac_array_y.append(-1 * (unnormalized_gradient[4] + unnormalized_gradient[7] + unnormalized_gradient[10] + unnormalized_gradient[13]))
+
+
+    ## Margin Z
+    out_array_z = []
+    jac_array_z = []
+    for delta in delta_pos_range_vec_x:
+        network_input = np.concatenate([
+            np.array([0.0, 0.0, 1.0]),
+            np.zeros(12),
+            np.array([0.44 , 0.34, -0.5 - delta]),
+            np.array([0.44 , -0.34, -0.5 - delta]),
+            np.array([-0.44, 0.34, -0.5 - delta]),
+            np.array([-0.44, -0.34, -0.5 - delta]),
+            np.array([0.6]),
+            np.array([0.0, 0.0, 1.0] * 4)
+        ])
+        network_input = dataset_handler.scale_input(network_input)
+        network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
+        output = network(network_input.float())
+        # output = network(torch.from_numpy(network_input).float()).item()
+        output = dataset_handler.scale_output(output)
+        out_array_z.append(output.detach().numpy()[:][0])
+        # Compute gradients of the output w.r.t. the input
+        output.backward(torch.ones_like(output))
+        unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
+        # jac_array_y.append(unnormalized_gradient[16])
+        jac_array_z.append(-1 * (unnormalized_gradient[17] + unnormalized_gradient[20] + unnormalized_gradient[23] + unnormalized_gradient[26]))
+        # jac_array_z.append(-1 * (unnormalized_gradient[5] + unnormalized_gradient[8] + unnormalized_gradient[11] + unnormalized_gradient[14]))
 
     # ## Margin X
     # out_array_x = []
@@ -145,7 +175,7 @@ def plot_learnt_margin():
     #         unnormalized_gradient[22] + unnormalized_gradient[25]))
 
 
-    return [[out_array_x, out_array_y], [jac_array_x, jac_array_y]]
+    return [[out_array_x, out_array_y, out_array_z], [jac_array_x, jac_array_y, jac_array_z]]
 
 
 ##############################################################################################
@@ -165,7 +195,8 @@ def plot_analytical_margin():
                           'FRICTION_AND_ACTUATION',
                           'FRICTION_AND_ACTUATION']
 
-    comWF = np.array([.0, 0.0, 0.0])
+    comWF = np.array([0.006, -0.002, -0.044])
+    comBF = np.array([0.006, -0.002, -0.044])
     comWF_lin_acc = np.array([.0, .0, .0])
     comWF_ang_acc = np.array([.0, .0, .0])
 
@@ -199,10 +230,10 @@ def plot_analytical_margin():
         informations needed for the computation of the IP'''
     params = IterativeProjectionParameters(robot_name)
     """ contact points in the World Frame"""
-    LF_foot = np.array([0.44, 0.34, -0.55])
-    RF_foot = np.array([0.44, -0.34, -0.55])
-    LH_foot = np.array([-0.44, 0.34, -0.55])
-    RH_foot = np.array([-0.44, -0.34, -0.55])
+    LF_foot = np.array([0.44, 0.34, -0.5])
+    RF_foot = np.array([0.44, -0.34, -0.5])
+    LH_foot = np.array([-0.44, 0.34, -0.5])
+    RH_foot = np.array([-0.44, -0.34, -0.5])
 
     contactsWF = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
 
@@ -226,6 +257,7 @@ def plot_analytical_margin():
     params.useInstantaneousCapturePoint = True
     params.externalCentroidalWrench = extCentroidalWrench
     params.setCoMPosWF(comWF)
+    params.setCoMPosBF(comBF)
     params.comLinVel = [0., 0.0, 0.0]
     params.setCoMLinAcc(comWF_lin_acc)
     params.setTorqueLims(comp_dyn.robotModel.robotModel.joint_torque_limits)
@@ -251,8 +283,8 @@ def plot_analytical_margin():
     params_foot_z = deepcopy(params)
     foot_id = 0
     margin_foot_pos_x, jac_foot_pos_x = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_x, 0)
-    margin_foot_pos_y, jac_foot_pos_y = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_y, 1)
-    margin_foot_pos_z, jac_foot_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_x, 2)
+    margin_foot_pos_y, jac_foot_pos_y = jac.plotMarginAndJacobianWrtComPosition(params_foot_y, delta_pos_range_vec_y, 1)
+    margin_foot_pos_z, jac_foot_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_foot_z, delta_pos_range_vec_x, 2)
 
 
     print("pos_margin_x:", margin_foot_pos_x)
@@ -279,43 +311,58 @@ def main():
     fig1 = plt.figure(1)
     fig1.suptitle("Stability margin")
 
-    plt.subplot(121)
-    plt.plot(delta_pos_range_vec_x, margin[0], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_x, margin_learnt[0], 'b', markersize=15, label='CoM')
+    plt.subplot(131)
+    plt.plot(delta_pos_range_vec_x, margin[0], 'g', markersize=15, label='numerical')
+    # plt.plot(delta_pos_range_vec_x, margin_learnt[0], 'b', markersize=15, label='learn (backprop)')
     plt.grid()
     plt.xlabel("$p_x$ [m]")
     plt.ylabel("m [m]")
     plt.title("LF X pos margin")
 
-    plt.subplot(122)
-    plt.plot(delta_pos_range_vec_y, margin[1], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_y, margin_learnt[1], 'b', markersize=15, label='CoM')
+    plt.subplot(132)
+    plt.plot(delta_pos_range_vec_y, margin[1], 'g', markersize=15, label='numerical')
+    # plt.plot(delta_pos_range_vec_y, margin_learnt[1], 'b', markersize=15, label='learn (backprop)')
     plt.grid()
     plt.xlabel("$p_y$ [m]")
     plt.ylabel("m [m]")
     plt.title("LF Y pos margin")
 
-    # ## Jacobians
-    # fig2 = plt.figure(2)
-    # fig2.suptitle("Gradients")
-    # print("jacobian", jacobian[0][0])
-    # print("jacobian_learnt:", jacobian_learnt[0])
+    plt.subplot(133)
+    plt.plot(delta_pos_range_vec_x, margin[2], 'g', markersize=15, label='numerical')
+    # plt.plot(delta_pos_range_vec_x, margin_learnt[2], 'b', markersize=15, label='learn (backprop)')
+    plt.grid()
+    plt.xlabel("$p_z$ [m]")
+    plt.ylabel("m [m]")
+    plt.title("LF Z pos margin")
 
-    # plt.subplot(221)
-    # plt.plot(delta_pos_range_vec_x, jacobian[0], 'g', markersize=15, label='learnt (backprop)')
+    ## Jacobians
+    fig2 = plt.figure(2)
+    fig2.suptitle("Gradients")
+    print("jacobian", jacobian[0][0])
+    print("jacobian_learnt:", jacobian_learnt[0])
+
+    plt.subplot(131)
+    plt.plot(delta_pos_range_vec_x, jacobian[0][0], 'g', markersize=15, label='numerical')
     # plt.plot(delta_pos_range_vec_x, jacobian_learnt[0], 'b', markersize=15, label='learn (backprop)')
-    # plt.grid()
-    # plt.xlabel("$p_x$ [m]")
-    # plt.ylabel("\delta m/ \delta p_{x}")
-    # # plt.title("CoM X pos margin")
+    plt.grid()
+    plt.xlabel("$c_x$ [m]")
+    plt.ylabel("\delta m/ \delta c_{x}")
+    # plt.title("CoM X pos margin")
 
-    # plt.subplot(222)
-    # plt.plot(delta_pos_range_vec_y, jacobian[1], 'g', markersize=15, label='learnt (backprop)')
+    plt.subplot(132)
+    plt.plot(delta_pos_range_vec_y, jacobian[1][1], 'g', markersize=15, label='numerical')
     # plt.plot(delta_pos_range_vec_y, jacobian_learnt[1], 'b', markersize=15, label='learnt (backprop)')
-    # plt.grid()
-    # plt.xlabel("$p_y$ [m]")
-    # plt.ylabel("\delta m/ \delta p_{y}")
-    # # plt.title("CoM Y pos margin")
+    plt.grid()
+    plt.xlabel("$c_y$ [m]")
+    plt.ylabel("\delta m/ \delta c_{y}")
+    # plt.title("CoM Y pos margin")
+
+    plt.subplot(133)
+    plt.plot(delta_pos_range_vec_x, jacobian[2][2], 'g', markersize=15, label='numerical')
+    # plt.plot(delta_pos_range_vec_x, jacobian_learnt[2], 'b', markersize=15, label='learnt (backprop)')
+    plt.grid()
+    plt.xlabel("$c_z$ [m]")
+    plt.ylabel("\delta m/ \delta c_{z}")
 
     plt.show()
 

jet_leg_common/examples/margin_jacobian/plot_learned_foot_kinematic_margin_jac_hyqreal_1011_LF_xy.py
@@ -1,4 +1,6 @@
+import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import time
 from copy import deepcopy
@@ -15,79 +17,62 @@ from jet_leg_common.jet_leg.optimization import nonlinear_projection
 from jet_leg_common.jet_leg.dynamics.computational_dynamics import ComputationalDynamics
 from jet_leg_common.jet_leg.computational_geometry.computational_geometry import ComputationalGeometry
 from jet_leg_common.jet_leg.computational_geometry.iterative_projection_parameters import IterativeProjectionParameters
-from jet_leg_common.jet_leg.optimization.jacobians_kinematics import KinematicJacobians
+from jet_leg_common.jet_leg.optimization.jacobians_kinematics_xy import KinematicJacobiansXY
 
 
 robot_name = 'hyqreal'
 
-num_of_tests = 25
-delta_pos_range_x = 0.6
+num_of_tests = 50
+delta_pos_range_x = 0.4
 delta_pos_range_y = 0.4
-delta_pos_range_vec_x = np.linspace(-delta_pos_range_x/2.0, delta_pos_range_x/2.0, num_of_tests)
-delta_pos_range_vec_y = np.linspace(-delta_pos_range_y/2.0, delta_pos_range_y/2.0, num_of_tests)
+delta_pos_range_vec_xy = np.array([np.linspace(-delta_pos_range_x/2.0, delta_pos_range_x/2.0, num_of_tests),
+                                   np.linspace(-delta_pos_range_y/2.0, delta_pos_range_y/2.0, num_of_tests)])
 
 def plot_learnt_margin():
 
     paths = ProjectPaths()
-    dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/0111', robot_name=robot_name, in_dim=34)
-    model_directory = '/hyqreal/seperate(kinematic)/0111/'
+    dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/1011', robot_name=robot_name, in_dim=34)
+    model_directory = '/hyqreal/seperate(kinematic)/1011/'
     model_directory = paths.TRAINED_MODELS_PATH + model_directory
     print("model directory: ", model_directory)
-    network = MultiLayerPerceptron(in_dim=34, out_dim=1, hidden_layers=[256,128,128], activation='softsign', dropout=0.0)
+    network = MultiLayerPerceptron(in_dim=34, out_dim=1, hidden_layers=[512,256,128], activation='relu', dropout=0.0)
     # network.load_state_dict(torch.load(model_directory + 'network_state_dict.pt'))
     network.load_params_from_txt(model_directory + 'network_parameters_hyqreal.txt')
     network.eval()
 
     ## Margin X
-    out_array_x = []
+    out_array_xy = np.zeros((num_of_tests, num_of_tests))
+    count_x = 0
     jac_array_x = []
-    for delta in delta_pos_range_vec_x:
-        network_input = np.concatenate([
-            np.array([0.0, 0.0, 1.0]),
-            np.zeros(12),
-            np.array([0.44 - delta, -0.34, -0.55]),
-            np.array([-0.44 - delta, 0.34, -0.55]),
-            np.array([-0.44 - delta, -0.34, -0.55]),
-            np.array([0.6]),
-            np.array([0.0, 0.0, 1.0] * 3)
-        ])
-        network_input = dataset_handler.scale_input(network_input)
-        network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
-        output = network(network_input.float())
-        # output = network(torch.from_numpy(network_input).float()).item()
-        output = dataset_handler.scale_output(output)
-        out_array_x.append(output.detach().numpy()[:][0])
-        # Compute gradients of the output w.r.t. the input
-        output.backward(torch.ones_like(output))
-        unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
-        jac_array_x.append(unnormalized_gradient[15])
-
-    ## Margin Y
-    out_array_y = []
-    jac_array_y = []
-    for delta in delta_pos_range_vec_y:
-        network_input = np.concatenate([
-            np.array([0.0, 0.0, 1.0]),
-            np.zeros(12),
-            np.array([0.44 , -0.34 - delta, -0.55]),
-            np.array([-0.44, 0.34 - delta, -0.55]),
-            np.array([-0.44, -0.34 - delta, -0.55]),
-            np.array([0.6]),
-            np.array([0.0, 0.0, 1.0] * 3)
-        ])
-        network_input = dataset_handler.scale_input(network_input)
-        network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
-        output = network(network_input.float())
-        # output = network(torch.from_numpy(network_input).float()).item()
-        output = dataset_handler.scale_output(output)
-        out_array_y.append(output.detach().numpy()[:][0])
-        # Compute gradients of the output w.r.t. the input
-        output.backward(torch.ones_like(output))
-        unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
-        jac_array_y.append(unnormalized_gradient[16])
+    for delta_x in delta_pos_range_vec_xy[0]:
+        count_y = 0
+        for delta_y in delta_pos_range_vec_xy[1]:
+            network_input = np.concatenate([
+                np.array([0.0, 0.0, 1.0]),
+                np.array([0., 0., 0.0]),
+                np.zeros(3),
+                np.zeros(3),
+                np.zeros(3),
+                np.array([0.44 + delta_x, 0.34 + delta_y, -0.55]),
+                np.array([-0.44, 0.34, -0.55]),
+                np.array([-0.44, -0.34, -0.55]),
+                np.array([0.6]),
+                np.array([0.0, 0.0, 1.0] * 3)
+            ])
+            network_input = dataset_handler.scale_input(network_input)
+            network_input = torch.tensor(network_input, requires_grad=True)
+            output = network(network_input.float())
+            output = dataset_handler.scale_output(output)
+            out_array_xy[count_x][count_y] = output.detach().numpy()[:][0]
+            # Compute gradients of the output w.r.t. the input
+            # output.backward(torch.ones_like(output))
+            # unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
+            # jac_array_x.append(unnormalized_gradient[15])
+            count_y += 1
+        count_x += 1
 
 
-    return [[out_array_x, out_array_y], [jac_array_x, jac_array_y]]
+    return out_array_xy
 
 
 ##############################################################################################
@@ -108,7 +93,7 @@ def plot_analytical_margin():
                           'FRICTION_AND_ACTUATION']
 
     comWF = np.array([.0, 0.0, 0.0])
-    comWF_lin_acc = np.array([.0, .0, .0])
+    comWF_lin_acc = np.array([-0.0, -0.0, .0])
     comWF_ang_acc = np.array([.0, .0, .0])
 
     ''' extForceW is an optional external pure force (no external torque for now) applied on the CoM of the robot.'''
@@ -120,7 +105,7 @@ def plot_analytical_margin():
     mu = 0.6
 
     ''' stanceFeet vector contains 1 is the foot is on the ground and 0 if it is in the air'''
-    stanceFeet = [0, 1, 1, 1]
+    stanceFeet = [1, 0, 1, 1]
 
 
     ''' now I define the normals to the surface of the contact points. By default they are all vertical now'''
@@ -180,85 +165,90 @@ def plot_analytical_margin():
     params.setTotalMass(comp_dyn.robotModel.robotModel.trunkMass)
     params.externalForceWF = extForceW  # params.externalForceWF is actually used anywhere at the moment
 
-    params_base_roll = deepcopy(params)
-    params_base_pitch = deepcopy(params)
-
-    jac = KinematicJacobians(robot_name)
-    comp_geom = ComputationalGeometry()
+    jac = KinematicJacobiansXY(robot_name)
 
 
     ''' Margin LF '''
-    params_foot_x = deepcopy(params)
-    params_foot_y = deepcopy(params)
-    params_foot_z = deepcopy(params)
+    params_foot_xy = deepcopy(params)
+
     foot_id = 0
-    margin_foot_pos_x, jac_foot_pos_x = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_x, 0)
-    margin_foot_pos_y, jac_foot_pos_y = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_y, 1)
-    margin_foot_pos_z, jac_foot_pos_z = jac.plotMarginAndJacobianWrtComPosition(params_foot_x, delta_pos_range_vec_x, 2)
+    margin_foot_pos_xy = jac.plotMarginWrtFootPosition(params_foot_xy, foot_id, delta_pos_range_vec_xy)
 
 
-    print("pos_margin_x:", margin_foot_pos_x)
-    print("pos_margin_y:", margin_foot_pos_y)
-    print("pos_margin_z:", margin_foot_pos_z)
-    print("jac_com_pos_x:", jac_foot_pos_x)
-    print("jac_com_pos_y:", jac_foot_pos_y)
-    print("jac_com_pos_z:", jac_foot_pos_z)
+    print("pos_margin_x:", margin_foot_pos_xy)
+    # print("jac_com_pos_x:", jac_foot_pos_x)
     
         
-    return [[margin_foot_pos_x, margin_foot_pos_y, margin_foot_pos_z], [jac_foot_pos_x, jac_foot_pos_y, jac_foot_pos_z]]
+    return margin_foot_pos_xy
 
 #####################################################################################################
 
 def main():
 
-    margin_learnt, jacobian_learnt = plot_learnt_margin()
+    margin_learnt = plot_learnt_margin()
     print("Computed learnt margin")
-    margin, jacobian = plot_analytical_margin()
+    margin = plot_analytical_margin()
     print("Computed analytical margin")
     ### Plotting
-
+    plt.rcParams.update({'font.size': 12})
+    # plt.subplots_adjust(wspace=0.5)
+    plt.subplots_adjust(top=0.85)
     ## X axis
     fig1 = plt.figure(1)
-    fig1.suptitle("Stability margin")
+    # ax1 = fig1.add_subplot(111, projection='3d')
+    ax1 = fig1.gca(projection='3d')
+    fig1.suptitle("Feasibility margin (1011)", x=0.565, y=1.00)
+    # delta_pos_range_vec_x = np.linspace(-7.delta_pos_range_x/2.0, -7.8 + delta_pos_range_x/2.0, num_of_tests)
+    # delta_pos_range_vec_y = np.linspace(-5.delta_pos_range_y/2.0, -5.1 + delta_pos_range_y/2.0, num_of_tests)
 
-    plt.subplot(121)
-    plt.plot(delta_pos_range_vec_x, margin[0], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_x, margin_learnt[0], 'b', markersize=15, label='CoM')
-    plt.grid()
-    plt.xlabel("$p_x$ [m]")
-    plt.ylabel("m [m]")
-    plt.title("LF X pos margin")
+    X, Y = np.meshgrid(delta_pos_range_vec_xy[0], delta_pos_range_vec_xy[1])
+    print("X:", X)
+    print("Y:", Y)
+    # print("margin:\n", margin)
 
-    plt.subplot(122)
-    plt.plot(delta_pos_range_vec_y, margin[1], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_y, margin_learnt[1], 'b', markersize=15, label='CoM')
-    plt.grid()
-    plt.xlabel("$p_y$ [m]")
-    plt.ylabel("m [m]")
-    plt.title("LF Y pos margin")
+    # Plot both surfaces on the same axes
+    ax1.plot_surface(X, Y, margin.T, color='g', alpha=0.7, rstride=1, cstride=1, label='numerical',linewidth=3.0)
+    ax1.plot_surface(X, Y, margin_learnt.T, color='b', alpha=0.6, rstride=1, cstride=1, label='learnt',linewidth=3.0)
+    ax1.set_xlabel("$p_x$ [m]")
+    ax1.set_ylabel("$p_y$ [m]")
+    ax1.set_zlabel("margin [m]")
+    ax1.set_title("LF X")
+
+    # plt.subplot(121)
+    # plt.plot(delta_pos_range_vec_xy, margin[0], 'g', markersize=15, label='analytical',linewidth=3.0)
+    # plt.plot(delta_pos_range_vec_xy, margin_learnt[0], 'b', markersize=15, label='learnt',linewidth=3.0)
+    # plt.grid()
+    # plt.xlabel("$p_x$ [m]")
+    # plt.ylabel("margin [m]")
+    # plt.title("LF X")
+    
 
     # ## Jacobians
     # fig2 = plt.figure(2)
     # fig2.suptitle("Gradients")
-    # print("jacobian", jacobian[0][0])
+    # print("jacobian", jacobian[0])
     # print("jacobian_learnt:", jacobian_learnt[0])
 
-    # plt.subplot(221)
-    # plt.plot(delta_pos_range_vec_x, jacobian[0], 'g', markersize=15, label='learnt (backprop)')
-    # plt.plot(delta_pos_range_vec_x, jacobian_learnt[0], 'b', markersize=15, label='learn (backprop)')
+    # plt.subplot(121)
+    # plt.plot(delta_pos_range_vec_x, jacobian[0], 'g', markersize=15, label='analytical',linewidth=3.0)
+    # plt.plot(delta_pos_range_vec_x, jacobian_learnt[0], 'b', markersize=15, label='learnt',linewidth=3.0)
     # plt.grid()
-    # plt.xlabel("$p_x$ [m]")
-    # plt.ylabel("\delta m/ \delta p_{x}")
+    # plt.xlabel("$c_x$ [m]")
+    # plt.ylabel("\delta m/ \delta c_{x}")
     # # plt.title("CoM X pos margin")
 
-    # plt.subplot(222)
-    # plt.plot(delta_pos_range_vec_y, jacobian[1], 'g', markersize=15, label='learnt (backprop)')
-    # plt.plot(delta_pos_range_vec_y, jacobian_learnt[1], 'b', markersize=15, label='learnt (backprop)')
+    # plt.subplot(122)
+    # plt.plot(delta_pos_range_vec_y, jacobian[1], 'g', markersize=15, label='analytical',linewidth=3.0)
+    # plt.plot(delta_pos_range_vec_y, jacobian_learnt[1], 'b', markersize=15, label='learnt',linewidth=3.0)
     # plt.grid()
-    # plt.xlabel("$p_y$ [m]")
-    # plt.ylabel("\delta m/ \delta p_{y}")
+    # plt.xlabel("$c_y$ [m]")
+    # plt.ylabel("\delta m/ \delta c_{y}")
     # # plt.title("CoM Y pos margin")
 
+    # plt.tight_layout()
+    # plt.ylim(top=2.0)
+    # fig1.tight_layout()
+    # fig2.tight_layout()
     plt.show()
 
 

jet_leg_common/examples/margin_jacobian/plot_learned_orientation_kinematic_margin_hyqreal_0111.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 from copy import deepcopy
+from scipy.spatial.transform import Rotation as R
+from scipy.optimize import approx_fprime
 
 import torch
 import torch.nn.functional as F
@@ -21,21 +23,37 @@ from jet_leg_common.jet_leg.optimization.jacobians_kinematics import KinematicJa
 robot_name = 'hyqreal'
 
 num_of_tests = 25
-delta_pos_range_x = 0.6
+delta_pos_range_x = 0.4
 delta_pos_range_y = 0.4
-delta_pos_range_vec_x = np.linspace(-delta_pos_range_x/2.0, delta_pos_range_x/2.0, num_of_tests)
-delta_pos_range_vec_y = np.linspace(-delta_pos_range_y/2.0, delta_pos_range_y/2.0, num_of_tests)
+delta_pos_range_z = 0.4
+delta_orient_range_vec_x = np.linspace(-delta_pos_range_x/2.0, delta_pos_range_x/2.0, num_of_tests)
+delta_orient_range_vec_y = np.linspace(-delta_pos_range_y/2.0, delta_pos_range_y/2.0, num_of_tests)
+delta_orient_range_vec_z = np.linspace(-delta_pos_range_z/2.0, delta_pos_range_z/2.0, num_of_tests)
+gradient_epsilon = 1e-8
+
+def g(euler):
+
+    g = np.array([
+        np.sin(euler[0])*np.sin(euler[2]) - np.cos(euler[0])*np.cos(euler[2])*np.sin(euler[1]),
+        np.cos(euler[2])*np.sin(euler[0]) + np.cos(euler[0])*np.sin(euler[1])*np.sin(euler[2]),
+        np.cos(euler[0])*np.cos(euler[1])
+    ])
+
+    return g
+
+def g_component(euler, index):
+    return g(euler)[index]
 
 def plot_learnt_margin():
 
     paths = ProjectPaths()
-    dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/1111', robot_name=robot_name)
-    model_directory = '/hyqreal/seperate(kinematic)/1111/'
+    dataset_handler = TrainingDataset('../trained_models/hyqreal/seperate(kinematic)/0111', robot_name=robot_name, in_dim=34)
+    model_directory = '/hyqreal/seperate(kinematic)/0111/'
     # dataset_handler = TrainingDataset('../trained_models/final/stability_margin', robot_name=robot_name)
     # model_directory = '/final/stability_margin/'
     model_directory = paths.TRAINED_MODELS_PATH + model_directory
     print("model directory: ", model_directory)
-    network = MultiLayerPerceptron(in_dim=40, out_dim=1, hidden_layers=[256,256,128], activation='relu', dropout=0.0)
+    network = MultiLayerPerceptron(in_dim=34, out_dim=1, hidden_layers=[256,128,128], activation='softsign', dropout=0.0)
     # network.load_state_dict(torch.load(model_directory + 'network_state_dict.pt'))
     network.load_params_from_txt(model_directory + 'network_parameters_hyqreal.txt')
     network.eval()
@@ -43,16 +61,23 @@ def plot_learnt_margin():
     ## Margin LF X
     out_array_x = []
     jac_array_x = []
-    for delta in delta_pos_range_vec_x:
+    for delta in delta_orient_range_vec_x:
+
+        # Compute rotation matrix (from base to world) from roll/pitch/yaw - Intrinsic rotations.
+        euler_angles = np.array([delta, 0.0, 0.0])
+        print("euler_angles:", euler_angles)
+        rotation_matrix = R.from_euler('XYZ', euler_angles, degrees=False).as_dcm() #
+        print("rotation_matrix[2]:", rotation_matrix[2])
+        print("g(euler_angles):", g(euler_angles))
+        # g = rotation_matrix[:, 2]
         network_input = np.concatenate([
-            np.array([0.0, 0.0, 1.0]),
+            rotation_matrix[2],
             np.zeros(12),
-            np.array([0.44 + delta, 0.34, -0.55]),
             np.array([0.44, -0.34, -0.55]),
             np.array([-0.44, 0.34, -0.55]),
             np.array([-0.44, -0.34, -0.55]),
             np.array([0.6]),
-            np.array([0.0, 0.0, 1.0] * 4)
+            np.array([0.0, 0.0, 1.0] * 3)
         ])
         network_input = dataset_handler.scale_input(network_input)
         network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
@@ -62,22 +87,32 @@ def plot_learnt_margin():
         out_array_x.append(output.detach().numpy()[:][0])
         # Compute gradients of the output w.r.t. the input
         output.backward(torch.ones_like(output))
+
         unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
-        jac_array_x.append(unnormalized_gradient[15])
+        jacobian_g_euler = np.zeros((3, 3))
+        for i in range(3):
+            jacobian_g_euler[:, i] = approx_fprime(euler_angles, lambda x: g_component(x, i), gradient_epsilon)
+        print("jacobian_g_euler:", jacobian_g_euler)
+        gradient = unnormalized_gradient[0:3]
+        # rotation_matrices
+        # jac_array_x.append(unnormalized_gradient[15])
+        jac_array_x.append(unnormalized_gradient[0])
 
     ## Margin LF Y
     out_array_y = []
     jac_array_y = []
-    for delta in delta_pos_range_vec_y:
+    for delta in delta_orient_range_vec_y:
+        # Compute rotation matrix (from base to world) from roll/pitch/yaw - Intrinsic rotations.
+        rotation_matrix = R.from_euler('XYZ', np.array([0.0, delta, 0.0]), degrees=False).as_dcm() #
+        # g = rotation_matrix[:, 2]
         network_input = np.concatenate([
-            np.array([0.0, 0.0, 1.0]),
+            rotation_matrix[2],
             np.zeros(12),
-            np.array([0.44 , 0.34 + delta, -0.55]),
             np.array([0.44 , -0.34, -0.55]),
             np.array([-0.44, 0.34, -0.55]),
             np.array([-0.44, -0.34, -0.55]),
             np.array([0.6]),
-            np.array([0.0, 0.0, 1.0] * 4)
+            np.array([0.0, 0.0, 1.0] * 3)
         ])
         network_input = dataset_handler.scale_input(network_input)
         network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
@@ -88,59 +123,8 @@ def plot_learnt_margin():
         # Compute gradients of the output w.r.t. the input
         output.backward(torch.ones_like(output))
         unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
+        # jac_array_y.append(unnormalized_gradient[16])
         jac_array_y.append(unnormalized_gradient[16])
-
-    # ## Margin X
-    # out_array_x = []
-    # jac_array_x = []
-    # for delta in delta_pos_range_vec_x:
-    #     network_input = np.concatenate([
-    #         np.array([0.0, 0.0, 1.0]),
-    #         np.zeros(12),
-    #         np.array([0.44 - delta, 0.34, -0.55]),
-    #         np.array([0.44 - delta, -0.34, -0.55]),
-    #         np.array([-0.44 - delta, 0.34, -0.55]),
-    #         np.array([-0.44 - delta, -0.34, -0.55]),
-    #         np.array([0.5]),
-    #         np.array([0.0, 0.0, 1.0] * 4)
-    #     ])
-    #     network_input = dataset_handler.scale_input(network_input)
-    #     network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
-    #     output = network(network_input.float())
-    #     # output = network(torch.from_numpy(network_input).float()).item()
-    #     output = dataset_handler.scale_output(output)
-    #     out_array_x.append(output.detach().numpy()[:][0])
-    #     # Compute gradients of the output w.r.t. the input
-    #     output.backward(torch.ones_like(output))
-    #     unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
-    #     jac_array_x.append(-1 * (unnormalized_gradient[15] + unnormalized_gradient[18] + \
-    #         unnormalized_gradient[21] + unnormalized_gradient[24]))
-
-    # ## Margin Y
-    # out_array_y = []
-    # jac_array_y = []
-    # for delta in delta_pos_range_vec_y:
-    #     network_input = np.concatenate([
-    #         np.array([0.0, 0.0, 1.0]),
-    #         np.zeros(12),
-    #         np.array([0.44 , 0.34 - delta, -0.55]),
-    #         np.array([0.44 , -0.34 - delta, -0.55]),
-    #         np.array([-0.44, 0.34 - delta, -0.55]),
-    #         np.array([-0.44, -0.34 - delta, -0.55]),
-    #         np.array([0.5]),
-    #         np.array([0.0, 0.0, 1.0] * 4)
-    #     ])
-    #     network_input = dataset_handler.scale_input(network_input)
-    #     network_input = torch.tensor(network_input, requires_grad=True) # requires_grad to record gradients w.r.t input
-    #     output = network(network_input.float())
-    #     # output = network(torch.from_numpy(network_input).float()).item()
-    #     output = dataset_handler.scale_output(output)
-    #     out_array_y.append(output.detach().numpy()[:][0])
-    #     # Compute gradients of the output w.r.t. the input
-    #     output.backward(torch.ones_like(output))
-    #     unnormalized_gradient = dataset_handler.unscale_gradient(network_input.grad.numpy())
-    #     jac_array_y.append(-1 * (unnormalized_gradient[16] + unnormalized_gradient[19] + \
-    #         unnormalized_gradient[22] + unnormalized_gradient[25]))
 
 
     return [[out_array_x, out_array_y], [jac_array_x, jac_array_y]]
@@ -176,7 +160,7 @@ def plot_analytical_margin():
     mu = 0.6
 
     ''' stanceFeet vector contains 1 is the foot is on the ground and 0 if it is in the air'''
-    stanceFeet = [1, 1, 1, 1]
+    stanceFeet = [0, 1, 1, 1]
 
 
     ''' now I define the normals to the surface of the contact points. By default they are all vertical now'''
@@ -244,24 +228,24 @@ def plot_analytical_margin():
 
 
     ''' Margin LF '''
-    params_foot_x = deepcopy(params)
-    params_foot_y = deepcopy(params)
-    params_foot_z = deepcopy(params)
+    params_euler_x = deepcopy(params)
+    params_euler_y = deepcopy(params)
+    params_euler_z = deepcopy(params)
     foot_id = 0
-    margin_foot_pos_x, jac_foot_pos_x = jac.plotMarginAndJacobianWrtFootPosition(params_foot_x, 0, delta_pos_range_vec_x, 0)
-    margin_foot_pos_y, jac_foot_pos_y = jac.plotMarginAndJacobianWrtFootPosition(params_foot_x, 0, delta_pos_range_vec_y, 1)
-    margin_foot_pos_z, jac_foot_pos_z = jac.plotMarginAndJacobianWrtFootPosition(params_foot_x, 0, delta_pos_range_vec_x, 2)
+    margin_euler_x, jac_euler_x = jac.plotMarginAndJacobianWrtBaseOrientation(params_euler_x, delta_orient_range_vec_x, 0)
+    margin_euler_y, jac_euler_y = jac.plotMarginAndJacobianWrtBaseOrientation(params_euler_y, delta_orient_range_vec_y, 1)
+    margin_euler_z, jac_euler_z = jac.plotMarginAndJacobianWrtBaseOrientation(params_euler_z, delta_orient_range_vec_z, 2)
 
 
-    print("pos_margin_x:", margin_foot_pos_x)
-    print("pos_margin_y:", margin_foot_pos_y)
-    print("pos_margin_z:", margin_foot_pos_z)
-    print("jac_com_pos_x:", jac_foot_pos_x)
-    print("jac_com_pos_y:", jac_foot_pos_y)
-    print("jac_com_pos_z:", jac_foot_pos_z)
+    print("pos_margin_x:", margin_euler_x)
+    print("pos_margin_y:", margin_euler_y)
+    print("pos_margin_z:", margin_euler_z)
+    print("jac_com_pos_x:", jac_euler_x)
+    print("jac_com_pos_y:", jac_euler_y)
+    print("jac_com_pos_z:", jac_euler_z)
     
         
-    return [[margin_foot_pos_x, margin_foot_pos_y, margin_foot_pos_z], [jac_foot_pos_x, jac_foot_pos_y, jac_foot_pos_z]]
+    return [[margin_euler_x, margin_euler_y, margin_euler_z], [jac_euler_x, jac_euler_y, jac_euler_z]]
 
 #####################################################################################################
 
@@ -278,42 +262,42 @@ def main():
     fig1.suptitle("Stability margin")
 
     plt.subplot(121)
-    plt.plot(delta_pos_range_vec_x, margin[0], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_x, margin_learnt[0], 'b', markersize=15, label='CoM')
+    plt.plot(delta_orient_range_vec_x, margin[0], 'g', markersize=15, label='CoM')
+    plt.plot(delta_orient_range_vec_x, margin_learnt[0], 'b', markersize=15, label='CoM')
     plt.grid()
-    plt.xlabel("$p_x$ [m]")
+    plt.xlabel("$theta_roll$ [m]")
     plt.ylabel("m [m]")
     plt.title("LF X pos margin")
 
     plt.subplot(122)
-    plt.plot(delta_pos_range_vec_y, margin[1], 'g', markersize=15, label='CoM')
-    plt.plot(delta_pos_range_vec_y, margin_learnt[1], 'b', markersize=15, label='CoM')
+    plt.plot(delta_orient_range_vec_y, margin[1], 'g', markersize=15, label='CoM')
+    plt.plot(delta_orient_range_vec_y, margin_learnt[1], 'b', markersize=15, label='CoM')
     plt.grid()
-    plt.xlabel("$p_y$ [m]")
+    plt.xlabel("$theta_pitch$ [m]")
     plt.ylabel("m [m]")
     plt.title("LF Y pos margin")
 
-    ## Jacobians
-    fig2 = plt.figure(2)
-    fig2.suptitle("Gradients")
-    print("jacobian", jacobian[0][0])
-    print("jacobian_learnt:", jacobian_learnt[0])
+    # ## Jacobians
+    # fig2 = plt.figure(2)
+    # fig2.suptitle("Gradients")
+    # print("jacobian", jacobian[0][0])
+    # print("jacobian_learnt:", jacobian_learnt[0])
 
-    plt.subplot(221)
-    plt.plot(delta_pos_range_vec_x, jacobian[0], 'g', markersize=15, label='learnt (backprop)')
-    plt.plot(delta_pos_range_vec_x, jacobian_learnt[0], 'b', markersize=15, label='learn (backprop)')
-    plt.grid()
-    plt.xlabel("$p_x$ [m]")
-    plt.ylabel("\delta m/ \delta p_{x}")
-    # plt.title("CoM X pos margin")
+    # plt.subplot(221)
+    # plt.plot(delta_pos_range_vec_x, jacobian[0][0], 'g', markersize=15, label='learnt (backprop)')
+    # plt.plot(delta_pos_range_vec_x, jacobian_learnt[0], 'b', markersize=15, label='learn (backprop)')
+    # plt.grid()
+    # plt.xlabel("$p_x$ [m]")
+    # plt.ylabel("\delta m/ \delta p_{x}")
+    # # plt.title("CoM X pos margin")
 
-    plt.subplot(222)
-    plt.plot(delta_pos_range_vec_y, jacobian[1], 'g', markersize=15, label='learnt (backprop)')
-    plt.plot(delta_pos_range_vec_y, jacobian_learnt[1], 'b', markersize=15, label='learnt (backprop)')
-    plt.grid()
-    plt.xlabel("$p_y$ [m]")
-    plt.ylabel("\delta m/ \delta p_{y}")
-    # plt.title("CoM Y pos margin")
+    # plt.subplot(222)
+    # plt.plot(delta_pos_range_vec_y, jacobian[1][1], 'g', markersize=15, label='learnt (backprop)')
+    # plt.plot(delta_pos_range_vec_y, jacobian_learnt[1], 'b', markersize=15, label='learnt (backprop)')
+    # plt.grid()
+    # plt.xlabel("$p_y$ [m]")
+    # plt.ylabel("\delta m/ \delta p_{y}")
+    # # plt.title("CoM Y pos margin")
 
     plt.show()
 

jet_leg_common/jet_leg/optimization/jacobians_kinematics.py
@@ -10,7 +10,7 @@ class KinematicJacobians:
         self.projection = nonlinear_projection.NonlinearProjectionBretl(robot_name)
         self.compGeom = ComputationalGeometry()
         self.compDyn = ComputationalDynamics(robot_name)
-        self.delta = 0.001
+        self.delta = 0.01
 
     
     def computeComPosJacobian(self, params, contactsWF):
@@ -22,12 +22,17 @@ class KinematicJacobians:
             feet_pos_dim -= self.delta / 2.0
             feet_pos_WF_plus = copy.deepcopy(default)
             feet_pos_WF_plus[:, j] = feet_pos_dim
-            isPointFeasible, margin1 = self.computeCoMKinematicMargin(params, feet_pos_WF_plus)
+            isPointFeasible_fr, margin_fr1 = self.computeComMargin(params, feet_pos_WF_plus)
+            isPointFeasible, margin_kr1 = self.computeCoMKinematicMargin(params, feet_pos_WF_plus)
+            margin1 = min(margin_fr1, margin_kr1)
+            params.setContactsPosWF(default)
             feet_pos_dim = copy.deepcopy(default[:, j])
             feet_pos_dim += self.delta / 2.0
             feet_pos_WF_minus = copy.deepcopy(default)
             feet_pos_WF_minus[:, j] = feet_pos_dim
-            isPointFeasible, margin2 = self.computeCoMKinematicMargin(params, feet_pos_WF_minus)
+            isPointFeasible_fr, margin_fr2 = self.computeComMargin(params, feet_pos_WF_minus)
+            isPointFeasible, margin_kr2 = self.computeCoMKinematicMargin(params, feet_pos_WF_minus)
+            margin2 = min(margin_fr2, margin_kr2)
             diff = margin1 - margin2
             jacobian[j] = diff / (1.0*self.delta)
 
@@ -53,20 +58,43 @@ class KinematicJacobians:
 
     def computeComEulerAnglesJacobian(self, params):
         jacobian = np.zeros(3)
-        initiaPoint = params.getOrientation()
 
         for j in np.arange(0, 3):
+            initiaPoint = copy.copy(params.getOrientation())
             eulerAngles = initiaPoint
             eulerAngles[j] = initiaPoint[j] + self.delta / 2.0
             params.setEulerAngles(eulerAngles)
-            isPointFeasible, margin1 = self.compDyn.compute_IP_margin(params, "COM")
+            isPointFeasible, margin_fr1 = self.compDyn.compute_IP_margin(params, "COM")
+            com_check = params.getCoMPosWF()
+            polygon, computation_time = self.projection.project_polytope(params, com_check, 10. * np.pi / 180, 0.001)
+            margin_kr1 = 0
+            if polygon.size > 0 and np.any(polygon):
+                sPolygon = Polygon(polygon[:-1,:2])
+                sPoint = Point(com_check[:2])
+                margin_kr1 = sPoint.distance(sPolygon.exterior)
+                isPointFeasible = True
+                print("dist", margin_kr1)
+            margin1 = min(margin_fr1, margin_kr1)
 
+            initiaPoint = copy.copy(params.getOrientation())
             eulerAngles = initiaPoint
             eulerAngles[j] = initiaPoint[j] - self.delta / 2.0
             params.setEulerAngles(eulerAngles)
-            isPointFeasible, margin2 = self.compDyn.compute_IP_margin(params, "COM")
+            isPointFeasible, margin_fr2 = self.compDyn.compute_IP_margin(params, "COM")
+            com_check = params.getCoMPosWF()
+            polygon, computation_time = self.projection.project_polytope(params, com_check, 10. * np.pi / 180, 0.001)
+            margin_kr2 = 0
+            if polygon.size > 0 and np.any(polygon):
+                sPolygon = Polygon(polygon[:-1,:2])
+                sPoint = Point(com_check[:2])
+                margin_kr2 = sPoint.distance(sPolygon.exterior)
+                isPointFeasible = True
+                print("dist", margin_kr2)
+            margin2 = min(margin_fr2, margin_kr2)
+
             diff = margin1 - margin2
             jacobian[j] = diff / self.delta
+            print("Jacobian", jacobian[j])
 
         return jacobian
 
@@ -109,14 +137,41 @@ class KinematicJacobians:
         initialContacts = copy.deepcopy(params.getContactsPosWF())
         initiaPoint = copy.deepcopy(initialContacts[footID])
         for j in np.arange(0, 3):
-            contacts = initialContacts
+            contacts = copy.deepcopy(initialContacts)
+            print("contacts", contacts)
             contacts[footID,j] = initiaPoint[j] + self.delta / 2.0
-            isPointFeasible, margin1 = self.computeCoMKinematicMargin(params, contacts)
-            contacts = initialContacts
+            print("contacts1", contacts)
+            isPointFeasible_fr, margin_fr1 = self.computeComMargin(params, contacts)
+            isPointFeasible, margin_kr1 = self.computeCoMKinematicMargin(params, contacts)
+            margin1 = min(margin_fr1, margin_kr1)
+            print("margin1", margin1)
+            contacts = copy.deepcopy(initialContacts)
             contacts[footID, j] = initiaPoint[j] - self.delta / 2.0
-            isPointFeasible, margin2 = self.computeCoMKinematicMargin(params, contacts)
+            print("contacts2", contacts)
+            isPointFeasible_fr, margin_fr2 = self.computeComMargin(params, contacts)
+            isPointFeasible, margin_kr2 = self.computeCoMKinematicMargin(params, contacts)
+            margin2 = min(margin_fr2, margin_kr2)
+            print("margin2", margin2)
             diff = margin1 - margin2
             jacobian[j] = diff / self.delta
+
+        return jacobian
+    
+    def computeForceJacobian(self, params, margin_kr):
+        jacobian = np.zeros(3)
+        initialPoint = copy.deepcopy(params.getExternalForce())
+
+        for j in np.arange(0, 3):
+            params.externalForce = copy.deepcopy(initialPoint)
+            params.externalForce[j] = initialPoint[j] + self.delta / 2.0
+            isPointFeasible, margin_fr1 = self.compDyn.compute_IP_margin(params, "COM")
+            margin1 = min(margin_fr1, margin_kr)
+            params.externalForce = copy.deepcopy(initialPoint)
+            params.externalForce[j] = initialPoint[j] - self.delta / 2.0
+            isPointFeasible, margin_fr2 = self.compDyn.compute_IP_margin(params, "COM")
+            margin2 = min(margin_fr2, margin_kr)
+            diff = margin1 - margin2
+            jacobian[j] = diff / (1.0*self.delta)
 
         return jacobian
 
@@ -220,7 +275,30 @@ class KinematicJacobians:
 
         com_check = params.getCoMPosWF()
         params.setContactsPosWF(new_contacts_wf)
-        polygon, computation_time = self.projection.project_polytope(params, com_check, 20. * np.pi / 180, 0.03)    
+        print("new_contacts_wf", new_contacts_wf)
+        polygon, computation_time = self.projection.project_polytope(params, com_check, 10. * np.pi / 180, 0.001)
+        print("polygon", polygon)
+        if polygon.size > 0 and np.any(polygon):
+            sPolygon = Polygon(polygon[:-1,:2])
+            sPoint = Point(com_check[:2])
+            dist = sPoint.distance(sPolygon.exterior)
+            isPointFeasible = True
+            print("dist", dist)
+            if not sPolygon.contains(sPoint):
+                isPointFeasible = False
+                dist = -1 * dist
+            return isPointFeasible, dist
+        else:
+            print("Warning! IP failed.")
+            # Compute feet average position
+            center_point = np.mean(new_contacts_wf, axis=0)
+            margin = np.linalg.norm(center_point[:2] - com_check[:2]) - 0.3 # Should be value at nominal configuration
+            return False, margin
+    
+    def computeKinematicMargin(self, params):
+
+        com_check = params.getCoMPosWF()
+        polygon, computation_time = self.projection.project_polytope(params, com_check, 10. * np.pi / 180, 0.001)    
         if polygon.size > 0 and np.any(polygon):
             sPolygon = Polygon(polygon[:-1,:2])
             sPoint = Point(com_check[:2])
@@ -246,11 +324,12 @@ class KinematicJacobians:
             """ contact points in the World Frame"""
             contactsWF = copy.deepcopy(default_feet_pos_WF) 
             contactsWF[:, dim_to_check] -= delta
+            contactsWF_copy = copy.deepcopy(contactsWF) 
             isPointFeasible_fr, margin_fr = self.computeComMargin(params, contactsWF)
             isPointFeasible_kin, margin_kin = self.computeCoMKinematicMargin(params, contactsWF)
+            print("margin_fr", margin_fr)
             margin[count] = min(margin_fr, margin_kin)
             jac_com_pos[:, count] = self.computeComPosJacobian(params, contactsWF)
-
             count += 1
         print("margin: ", margin)
         params.setContactsPosWF(default_feet_pos_WF)
@@ -273,17 +352,20 @@ class KinematicJacobians:
 
         for delta in base_orient_range:
             eulerAngles = copy.deepcopy(default_euler_angles)
-            eulerAngles[dim_to_check] -= delta
+            eulerAngles[dim_to_check] += delta
             params.setOrientation(eulerAngles)
 
-            isPointFeasible, margin[count] = self.compDyn.compute_IP_margin(params, "COM")
+            isPointFeasible, margin_fr = self.compDyn.compute_IP_margin(params, "COM")
             print("params.eurlerAngles ", params.getOrientation())
+            isPointFeasible_kin, margin_kin = self.computeKinematicMargin(params)
+            margin[count] = min(margin_fr, margin_kin)
             print("margin ", margin[count])
-            marginJAcWrtBaseOrient = self.computeBaseOrientationJacobian(params)
-            print("margin jac wrt base orient", marginJAcWrtBaseOrient)
+            marginJAcWrtBaseOrient = self.computeComEulerAnglesJacobian(params)
             jac_base_orient[:, count] = marginJAcWrtBaseOrient
 
             count += 1
+        
+        params.setOrientation(default_euler_angles)
 
         return margin, jac_base_orient
 
@@ -296,17 +378,14 @@ class KinematicJacobians:
 
         for delta in foot_pos_range:
             # """ contact points in the World Frame"""
-            # LF_foot = np.array([0.3, 0.2, -0.4])
-            # RF_foot = np.array([0.3, -0.2, -0.4])
-            # LH_foot = np.array([-0.3, 0.2, -0.4])
-            # RH_foot = np.array([-0.3, -0.2, -0.4])
-
             # contactsWF = np.vstack((LF_foot, RF_foot, LH_foot, RH_foot))
             contactsWF = copy.deepcopy(default_feet_pos_WF)
             contactsWF[foot_id, dimension_to_check] += delta
             # params.setContactsPosWF(contactsWF)
             isPointFeasible_fr, margin_fr = self.computeComMargin(params, contactsWF)
+            print("margin_fr", margin_fr)
             isPointFeasible_kin, margin_kin = self.computeCoMKinematicMargin(params, contactsWF)
+            print("margin_kin", margin_kin)
             margin[count] = min(margin_fr, margin_kin)
             params.setContactsPosWF(contactsWF)
             marginJacWrtFootPos = self.computeFootJacobian(params, foot_id)
@@ -315,3 +394,26 @@ class KinematicJacobians:
             count += 1
 
         return margin, jac_foot_pos
+    
+    def plotMarginAndJacobianWrtForce(self, params, force_range, dim_to_check):
+        num_of_tests = np.shape(force_range)
+        margin = np.zeros(num_of_tests)
+        jac_force = np.zeros((3,num_of_tests[0]))
+        count = 0
+        default_force_WF = copy.deepcopy(params.getExternalForce())
+        isPointFeasible_kin, margin_kin = self.computeKinematicMargin(params)
+
+        for delta in force_range:
+            """ contact points in the World Frame"""
+            extForceWF = copy.deepcopy(default_force_WF)
+            extForceWF[dim_to_check] += delta
+            params.externalForce = extForceWF
+            isPointFeasible, margin_fr = self.compDyn.compute_IP_margin(params, "COM")
+            margin[count] = min(margin_fr, margin_kin)
+            marginJacWrtForce = self.computeForceJacobian(params, margin_kin)
+            jac_force[:, count] = marginJacWrtForce
+
+            count += 1
+        params.externalForce = default_force_WF
+
+        return margin, jac_force

scripts/utility/model_to_txt_converter.py
@@ -62,7 +62,7 @@ def load_hyperparameters_from_json(model_directory):
         with open(json_path, 'r') as f:
             hyperparemters = json.load(f)
         print("Loaded hyperparameters from:", json_path)
-        return hyperparemters
+        return hyperparemters, json_path
     except json.JSONDecodeError:
         raise ValueError("Error decoding JSON file at {}".format(json_path))
 
@@ -77,7 +77,7 @@ def main():
     print("Loading model", model_directory + os.listdir(model_directory)[0])
 
     # Load hyperparameters from the JSON file
-    hyperparameters = load_hyperparameters_from_json(model_directory)
+    hyperparameters, json_path = load_hyperparameters_from_json(model_directory)
     # Ensure that hyperparameters are present in the JSON file
     hidden_layers = hyperparameters['hidden_layers']
     activation_function = hyperparameters['activation_function']
@@ -108,6 +108,7 @@ def main():
     np.savetxt(param_save_name, model_parameters.reshape((1, -1)), delimiter=', ',
                newline='\n', fmt='%1.10f')
     copy_file(model_path, save_directory)
+    copy_file(json_path, save_directory)
 
     print('\nSaved model parameters in the following order:')
     for parameter_key in list(network.state_dict().keys()):